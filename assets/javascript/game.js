9ac685f506227fd4a8e355568aec4083dff50618
Justin Clark <justclark.jose@gmail.com>
2018-12-08 09:51:24 -0600

    changes made before class 12/8

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,18 @@
 var computerchoice = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
 
 //Creates variables to hold the number of wins losses and ties starting at 0.
-var wins = 0
-    losses = 0
-    ties = 0;
+var wins = 0;
+var losses = 0;
+var ties = 0;
 
+// Create variables placeholder references to the places in the HTML file.
+var directionstext = document.getElementById("directionstext")
+var wins = document.getElementById("wins")
+var losses = document.getElementById("losses")
+var remainingguesses = document.getElementById("remainingguesses")
+var guessedcharacters = document.getElementById("guessedcharacters")
+
+// This function is run whenever the user presses a key.
+document.onkeyup = function(event) {
+    
+}
