5591fa8750f94164db387a6d983535946a4e9b08
Justin Clark <justclark.jose@gmail.com>
2018-12-07 21:15:32 -0600

    third try

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//Array that lists out options for the game.
+var computerchoice = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+
+//Creates variables to hold the number of wins losses and ties starting at 0.
+var wins = 0
+    losses = 0
+    ties = 0;
+
