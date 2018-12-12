6483acfaafa58d3023dc9bf2e0a20541942178c0
Justin Clark <justclark.jose@gmail.com>
2018-12-11 19:39:14 -0600

    committing homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,52 @@
 //Array that lists out options for the game.
-var computerchoice = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+var computerChoice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 //Creates variables to hold the number of wins losses and ties starting at 0.
 var wins = 0;
 var losses = 0;
 var ties = 0;
+var numGuess = 9;
+var guessedChar = [];
 
 // Create variables placeholder references to the places in the HTML file.
-var directionstext = document.getElementById("directionstext")
-var wins = document.getElementById("wins")
-var losses = document.getElementById("losses")
-var remainingguesses = document.getElementById("remainingguesses")
-var guessedcharacters = document.getElementById("guessedcharacters")
+var directionsText = document.getElementById("directions-text")
+var winsText = document.getElementById("wins")
+var lossesText = document.getElementById("losses")
+var remainingGuessesText = document.getElementById("remainingguesses")
+var guessedCharactersText = document.getElementById("guessedcharacters")
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
+var userGuess = event.key;
+var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+
+
+if((userGuess === computerGuess)){
+    wins++
+    guessedChara = [];
+}
+
+
+
+if(userGuess != computerGuess){
+    numGuess--
+
+}
+
+if(numGuess === 0){
+    numGuess = 9;
+    losses ++
     
+
+
+}
+
+directionsText.textContent = "hello";
+winsText.textContent = "Wins: " + wins;
+lossesText.textContent = "Losses: " + losses;
+remainingGuessesText.textContent = "Remaining Guesses: " + numGuess;
+guessedCharactersText.textContent = "Characters Guessed: " + guessedChar;
+
+
 }
